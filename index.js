335e84c8b2184a413fa37b0d1d39184f341de400
kevin <kevin_33_6@hotmail.com>
2021-01-30 04:02:19 -0600

    Parsing URL: use instance URL instead url module parse method because is deprecated since node v 11. url was parsed and trimmed with regex to get path

--- index.js
+++ index.js
@@ -5,9 +5,34 @@
 
 // Dependencies
 const http = require('http')
+const url = require('url')
+
 
 // Server should respond all request with a string
-const server = http.createServer((req, res) => res.end('hello world\n'))
+const server = http.createServer((req, res) => { 
+    
+    //Get URL to parse it
+        
+    const baseURL = 'http://' + req.headers.host + '/';
+    const parsedURL = new URL(req.url, baseURL);
+    
+    
+    // const parsedURL = url.parse(req.url, true)
+    
+    // Get the path
+
+    const path = parsedURL.pathname
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'')
+    
+    // Send the response
+
+    res.end('hello world\n')
+
+    // Log the request path
+    console.log('Request received on path: '+ trimmedPath)
+
+})    
+
 
 // Start the server
 server.listen(3000, () => console.log('listening on port 3000'))
