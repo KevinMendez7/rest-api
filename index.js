a4fd3c510a9a949f90727d62885a012c7103d5c2
kevin <kevin_33_6@hotmail.com>
2021-01-30 04:05:34 -0600

    url module require removed because URL object is global

--- index.js
+++ index.js
@@ -5,27 +5,20 @@
 
 // Dependencies
 const http = require('http')
-const url = require('url')
-
 
 // Server should respond all request with a string
 const server = http.createServer((req, res) => { 
     
     //Get URL to parse it
-        
+    // baseURL was added because URL instance has a bug since localhost is not a valid URL    
     const baseURL = 'http://' + req.headers.host + '/';
     const parsedURL = new URL(req.url, baseURL);
     
-    
-    // const parsedURL = url.parse(req.url, true)
-    
     // Get the path
-
     const path = parsedURL.pathname
     const trimmedPath = path.replace(/^\/+|\/+$/g,'')
     
     // Send the response
-
     res.end('hello world\n')
 
     // Log the request path
