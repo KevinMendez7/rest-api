a4c498e96eb2d8bc1b768ac7d4a78d1f79b361e1
= <=>
2021-02-24 00:29:04 -0600

    index : server and worker modules initializer added. data : list function created. handler : list function removed(was added by mistake). server : index logic from server was passed to server file. worker background workers was created to make checks to validate up status on request to server

--- index.js
+++ index.js
@@ -4,110 +4,25 @@
 */
 
 // Dependencies
-const http = require('http')
-const https = require('https')
-const fs = require('fs')
-const { StringDecoder } = require('string_decoder');
-const { httpPort, httpsPort, envName } = require('./lib/config');
-const handler = require('./lib/handler');
-const { parseJSONtoObject } = require('./lib/helper');
+const server = require('./lib/server')
+const worker = require('./lib/worker')
 
-// Server should respond all request with a string
-const httpServer = http.createServer((req, res) => server(req,res))    
+// Declare the app
 
-const httpsOptions = {
-    key : fs.readFileSync('./https/key.pem'),
-    cert : fs.readFileSync('./https/cert.pem')
+const app = {}
+
+// Init function
+
+app.init = () => {
+    // Start the server
+    server.init()
+
+    // Start the workers
+    worker.init()
 }
 
-const httpsServer = https.createServer(httpsOptions , (req, res) => server(req,res))    
+// Execute
+app.init()
 
-
-// Start the server
-httpServer.listen(httpPort, () => console.log(`listening on port ${httpPort} and ${envName} mode`))
-
-httpsServer.listen(httpsPort, () => console.log(`listening on port ${httpsPort} and ${envName} mode`))
-
-const server = (req, res) => { 
-    
-    //Get URL to parse it
-    // baseURL was added because URL instance has a bug since localhost is not a valid URL    
-    const baseURL = 'http://' + req.headers.host + '/';
-    const parsedURL = new URL(req.url, baseURL);
-    
-    // Get the path
-    const path = parsedURL.pathname
-    const trimmedPath = path.replace(/^\/+|\/+$/g,'')
-    const params = parsedURL.searchParams        
-    
-    // Get the query strings as an object    
-    const queryStringObject = params.toString().length !== 0 ? JSON.parse('{"' + decodeURI(params)
-    .replace(/"/g, '\\"')
-    .replace(/&/g, '","')
-    .replace(/=/g,'":"') + '"}') : null
-
-    // Get the HTTP method
-    const method = req.method.toLowerCase()
-
-    // Get headers as an object
-    const headers = req.headers
-
-    // Get the payload if any
-    const decoder = new StringDecoder('utf-8')
-    var buffer = ''
-
-    req.on('data', data => buffer += decoder.write(data))
-
-    req.on('end', () => {
-
-        buffer += decoder.end()
-        
-        // Choose the handler this request should go to, if is not found use not found handler
-
-        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handler.notfound
-        // console.log('queryStringObject')
-        // console.log(queryStringObject)
-        const data = {
-            trimmedPath,
-            queryStringObject,
-            method,
-            headers,
-            payload : parseJSONtoObject(buffer ? buffer : '{}')
-        }
-
-        // Route the request to the handler specified in the router
-        chosenHandler(data, (statusCode, payload) => {
-
-            // Use the status code called back by the handler or default to 200
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200
-
-            //Use the payload called back by the handler or default to empty
-
-            // console.log('payload')
-            // console.log(payload)
-
-            payload = typeof(payload) == 'object' ? payload : {}
-
-            // Convert the payload to a string
-            const payloadString = JSON.stringify(payload)
-
-            // Return the response
-            res.setHeader('Content-Type','application/json')
-            res.writeHead(statusCode)            
-            res.end(payloadString)
-
-            // Log the request path
-            console.log('returning this ', statusCode, payloadString)    
-        })                    
-
-    })    
-
-}
-
-// Define a request router
-const router = {
-    'ping' : handler.ping,
-    'user' : handler.user,
-    'token' : handler.token,
-    'check' : handler.check
-}
+// Export the app
+module.exports = app
